17582e00f98b3df935b3d55ff4f7a578e0199fef
Ivan Muñoz <ivan.munoz.r1@outlook.com>
2019-10-20 16:33:01 +0000

    actualizacion

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -39,6 +39,27 @@ export class Home extends React.Component {
 		const contador = this.state.tareas.length;
 		const tareasVacio = this.state.tareas.length > 0;
 
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/tata", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				//console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//this.setState({ tareas: datos.label }); //here is were your code should start after the fetch finishes
+				console.log(data.label); //this will print on the console the exact object received from the server
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+
 		return (
 			<div className="container bg-light">
 				<div className="row">
@@ -46,6 +67,8 @@ export class Home extends React.Component {
 					<div className="col-6">
 						<div className="text-center mt-5">
 							<h1>todos</h1>
+							<br />
+							<p>usar este tag como prueba</p>
 							<Input
 								value={this.state.value}
 								controlCambio={this.controlaCambio}
